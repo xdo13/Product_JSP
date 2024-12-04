7e943f71a6701ec83821eff3515ec18297b05d38
xdo13 <xdo13@naver.com>
2024-12-04 17:57:00 +0900

    다중파일 폼 및 오류해결 및 단일파일 전송기능 추가

--- src/main/java/com/company/controller/uploadServlet.java
+++ src/main/java/com/company/controller/uploadServlet.java
@@ -1,33 +1,61 @@
 package com.company.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class uploadServlet
- */
-@WebServlet("/uploadServlet")
-public class uploadServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+@WebServlet("/upload.do")
+public class uploadServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		// 여기를 바꿔주면 다운받는 경로가 바뀜
+		String savePath = "upload";
+
+		// 최대 업로드 파일 크기 5MB로 제한
+		int uploadFileSizeLimit = 5 * 1024 * 1024;
+		String encType = "UTF-8";
+
+		ServletContext context = getServletContext();
+		String uploadFilePath = context.getRealPath(savePath);
+		System.out.println("서버상의 실제 디렉토리 :");
+		System.out.println(uploadFilePath);
+		try {
+			MultipartRequest multi = new MultipartRequest(request, // request 객체
+					uploadFilePath, // 서버상의 실제 디렉토리
+					uploadFileSizeLimit, // 최대 업로드 파일 크기
+					encType,
+					new DefaultFileRenamePolicy());
+			String fileName = multi.getFilesystemName("uploadFile");
+
+			if (fileName == null) { // 파일이 업로드 되지 않았을때
+				System.out.print("파일 업로드 되지 않았음");
+			} else { // 파일이 업로드 되었을 때
+				out.println("<br> 글쓴이 :" + multi.getParameter("name"));
+				out.println("<br> 제 목 :" + multi.getParameter("title"));
+				out.println("<br> 파일명 :" + fileName);
+			}
+		} catch (Exception e) {
+			System.out.print("예외발생: " + e);
+		}
+
 	}
 
 }
