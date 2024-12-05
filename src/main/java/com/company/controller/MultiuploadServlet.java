c93e6bd75ce23f9af3e4733667d984d36391f2a0
xdo13 <xdo13@naver.com>
2024-12-05 16:38:53 +0900

    다중파일 업로드 추가

--- src/main/java/com/company/controller/MultiuploadServlet.java
+++ src/main/java/com/company/controller/MultiuploadServlet.java
@@ -1,33 +1,62 @@
 package com.company.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MultiuploadServlet
- */
-@WebServlet("/MultiuploadServlet")
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+
+@WebServlet("/upload2.do")
 public class MultiuploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String savePath = "upload";
+		int uploadFileSizeLimit = 5 * 1024 * 1024;
+		String encType = "UTF-8";
+		
+		ServletContext context= getServletContext();
+		String uploadFilePath = context.getRealPath(savePath);
+		
+		try {
+			MultipartRequest multi = new MultipartRequest(request, uploadFilePath, uploadFileSizeLimit, encType, new DefaultFileRenamePolicy());
+			Enumeration files = multi.getFileNames();
+			
+			while(files.hasMoreElements()) {
+				String file = (String) files.nextElement();
+				String file_name = multi.getFilesystemName(file);
+				
+				String ori_file_name = multi.getFilesystemName(file);
+				out.print("<br> 업로드된 파일명 : "+ file_name);
+				out.print("<br> 원본 파일명 : "+ ori_file_name);
+				out.print("<hr>");
+			}
+		} catch (Exception e) {
+			System.out.println("예외처리 발생 : " + e);
+			
+		}
+		
+
+
 	}
 
 }
