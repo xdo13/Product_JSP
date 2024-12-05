73024c851173ee9cbc420b677b0abca87e9433b4
xdo13 <xdo13@naver.com>
2024-12-05 17:47:39 +0900

    상품 리스트 관리자 페이지 폼 구현

---
+++ src/main/java/com/company/controller/ProductListServlet.java
@@ -0,0 +1,37 @@
+package com.company.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.company.dao.ProductDAO;
+import com.company.dto.ProductVO;
+
+
+@WebServlet("/productList.do")
+public class ProductListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProductDAO pDao = ProductDAO.getInstance();
+		List<ProductVO> productList = pDao.selectAllProducts();
+		request.setAttribute("productList", productList);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("product/productList.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
--- src/main/java/com/company/dao/ProductDAO.java
+++ src/main/java/com/company/dao/ProductDAO.java
@@ -1,5 +1,15 @@
 package com.company.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.company.dto.ProductVO;
+
+import util.DBManager;
+
 public class ProductDAO {
 	public ProductDAO() {
 	}
@@ -8,5 +18,32 @@ public class ProductDAO {
 	public static ProductDAO getInstance() {
 		return instance;
 	}
-
+	
+	public List<ProductVO> selectAllProducts(){
+		//최근 등록한 상품 먼저 출력
+		String sql = "select * from product order by code desc";
+		List<ProductVO> list = new ArrayList<ProductVO>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				ProductVO pVo = new ProductVO();
+				pVo.setCode(rs.getInt("code"));
+				pVo.setProductName(rs.getString("productName"));
+				pVo.setPrice(rs.getInt("price"));
+				pVo.setPictureUrl(rs.getString("pictureUrl"));
+				pVo.setDescription(rs.getString("description"));
+				list.add(pVo);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		return list;
+	}
 }
